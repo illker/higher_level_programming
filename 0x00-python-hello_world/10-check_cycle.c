dc0c3843b3de7bbca013d9ab22185a18e505db8e
David Alzate <illker@hotmail.com>
2020-12-07 22:01:58 +0000

    :rabbit: :turtle:

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "list.h"
+
+/**
+ * check_cycle - Linked list cycle 
+ * @list: list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	for (; slow && fast && fast->next;)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
