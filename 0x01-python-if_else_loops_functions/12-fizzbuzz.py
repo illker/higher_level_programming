c25729e4227466755cb899c0ceaeb4129f1ad967
David Alzate <illker@hotmail.com>
2020-12-09 04:02:29 +0000

    Fizz Buzz :chart_with_upwards_trend:

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,6 +5,6 @@ def fizzbuzz():
             print("Fizz", end="")
         if n % 5 == 0:
             print("Buzz", end="")
-        if n % 3 and i % 5:
+        if n % 3 and n % 5:
             print("{:d}".format(n), end="")
         print(end=" ")
