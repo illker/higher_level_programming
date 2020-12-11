5c64a23dc4f5351e8cec7d169b803aa1dbce5f83
David Alzate <illker@hotmail.com>
2020-12-11 00:53:24 +0000

    infinite add :loop:

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    add = 0	
+    add = 0
     for n in range(1, len(sys.argv)):
         add = add + int(sys.argv[n])
     print(add)
