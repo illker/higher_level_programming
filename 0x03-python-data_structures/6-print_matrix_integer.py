fef2cbf68fd4d4a9e720aa760d0c047295fc70ee
David Alzate <illker@hotmail.com>
2020-12-15 23:18:55 +0000

    Matrix :dragon_face:

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for n in matrix:
         x = ""
         for c in n:
-            print("{:s}{:d}".format(x, c),end="")
+            print("{:s}{:d}".format(x, c), end="")
             x = " "
         print()
