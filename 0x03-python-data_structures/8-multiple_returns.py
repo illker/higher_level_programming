0697a2aeb1e3fe581b5a27ae9ce91789491a18af
David Alzate <illker@hotmail.com>
2020-12-16 01:49:52 +0000

    returns :dolphin:

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence:
-        returns len(sentence), sentence[0]
+        returns (len(sentence), sentence[0])
     else:
-        returns len(sentence), None
+        returns (len(sentence), None)
