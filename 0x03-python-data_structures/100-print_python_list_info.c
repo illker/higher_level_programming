b9ca02c2614960e1a9d0014aaa7c18850dc9250a
David Alzate <illker@hotmail.com>
2020-12-16 05:01:56 +0000

    Python lists :panda_face:

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -17,7 +17,7 @@ void print_python_list_info(PyObject *p)
 	printf("[*] Size of the Python List = %d\n", size);
 	printf("[*] Allocated = %d\n", alloc);
 
-	for (i = 0, i < size, i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Element %d: ", i);
 		obj = PyList_GetItem(p, i);
