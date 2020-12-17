9ba8f3e6b20e6737626781ef8aad69fc90951a42
David Alzate <illker@hotmail.com>
2020-12-17 22:11:04 +0000

    pow matrix :electric_plug:

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     matrixf = []
     for n in matrix:
         matrixc = []
-        for x in n
+        for x in n:
             matrixc.append(pow(x, 2))
         matrixf.append(matrixc)
     return matrixf
