8b7d8fa01a9cae40c58f85ba90bd1dfcb6fbfa0e
David Alzate <illker@hotmail.com>
2020-12-18 03:00:14 +0000

    sorted dic :bookmark_tabs:

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     n = sorted(a_dictionary.keys())
     for x in n:
-        print("{:s}: {}".format(x, a_dictionary))
+        print("{:s}: {}".format(x, a_dictionary[x]))
