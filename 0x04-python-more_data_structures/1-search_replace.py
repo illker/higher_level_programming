b13036a2e3eba7b91af2bc43d9edd583cdf9c29a
David Alzate <illker@hotmail.com>
2020-12-18 01:09:51 +0000

    search :mailbox_with_no_mail:

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    matrixc = list(map(lambda x: replace if x == search else x, my_list))
-    return matrixc
+    return = list(map(lambda x: replace if x == search else x, my_list))
