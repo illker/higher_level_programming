0b12f9e4339d02d9da71e3dbd15a020f81b80d8f
David Alzate <illker@hotmail.com>
2021-01-06 05:52:50 +0000

    Coordinates :dart:

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,14 +6,11 @@ class Square:
     """method init size"""
 
     def __init__(self, size=0, position=(0, 0)):
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
         self.position = position
 
     """property"""
+    
     @property
     def size(self):
         return self.__size
