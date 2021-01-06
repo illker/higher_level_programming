4b205306e6e175020b49a8d8c05bf8dc8e238921
David Alzate <illker@hotmail.com>
2021-01-06 02:23:02 +0000

    square :floppy_disk:

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Def class Square with size"""
+
+
+class Square:
+    """method init size"""
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size > 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
