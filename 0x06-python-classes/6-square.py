7fddd0eae5ce5eaf9d3fa98485c3cc125ec09c9d
David Alzate <illker@hotmail.com>
2021-01-07 02:12:34 +0000

    Coordinates í¾¯:pushpin:

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,8 +8,8 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         """method init size"""
 
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     """property"""
 
