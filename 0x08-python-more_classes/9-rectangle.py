26c009f3ea24196f783005edacfb0e3acb632d2f
David Alzate <illker@hotmail.com>
2021-01-11 23:50:57 +0000

    try :cookie:

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -73,7 +73,7 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
-        elif rect_1.area() < rect_2.area():
+        if rect_1.area() < rect_2.area():
             return rect_2
 
     @classmethod
