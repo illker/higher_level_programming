7a04dd33abcecba378138a38aec5e04f5adf4774
David Alzate <illker@hotmail.com>
2021-01-11 23:05:25 +0000

    fix print format :fries:

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -54,4 +54,4 @@ class Rectangle:
             return str_rect[:-1]
 
     def __repr__(self):
-        return f"Rectangle ({self.__width}, {self.__height})"
+        return "Rectangle({}, {})".format(self.width, self.height)
