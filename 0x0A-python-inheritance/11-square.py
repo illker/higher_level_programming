6dcd8e60c18fb55557bfd2770b720bc8f5fd11bd
David Alzate <illker@hotmail.com>
2021-01-20 04:31:41 +0000

    inheritance :monkey:

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Square #2"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square"""
+
+    def __init__(self, size):
+        """method init"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """method area"""
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        """method str"""
+        burger = "[Square] {}/{}".format(self.__size, self.__size)
+        return burger
