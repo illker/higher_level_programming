6bfdc4177882391f8d3e0b4a99fc7953257aba00
David Alzate <illker@hotmail.com>
2021-01-20 03:52:03 +0000

    inheritance :monkey:

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Improve Geometry"""
+
+
+class BaseGeometry:
+    """class base geometry"""
+
+    def area(self):
+        """method area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """method int validator"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
