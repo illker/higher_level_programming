90f6ff93f1c5c5b055e31d75c9c4ef4da1e63e1e
David Alzate <illker@hotmail.com>
2021-01-21 04:04:15 +0000

    class :pig2:

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,7 +5,7 @@
 class Student():
     """returns the dictionary"""
 
-    def __init__(first_name, last_name, age):
+    def __init__(self, first_name, last_name, age):
         """method init"""
         self.first_name = first_name
         self.last_name = last_name
