a38fba6eb8537f843c31112c7ef87397fbb77681
David Alzate <illker@hotmail.com>
2021-01-21 03:42:41 +0000

    Load, add, save :rabbit:

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,8 +5,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 try:
-    l = load_from_json_file("add_item.json")
+    n = load_from_json_file("add_item.json")
 except:
-    l = []
-l.extend(sys.argv[1:])
-save_to_json_file(l, "add_item.json")
+    n = []
+for a in range(1, len(sys.argv)):
+    new_list.append(sys.argv[a])
+save_to_json_file(new_list, "add_item.json")
