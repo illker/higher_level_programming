fa8b7baf04543d74c62ebc4f609628f45f69b0fd
David Alzate <illker@hotmail.com>
2021-01-21 03:16:02 +0000

    Load, add, save :rabbit:

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Load, add, save"""
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json').load_from_json_file
+
+try:
+    n = load_from_json_file(add_item.json)
+except:
+    n = []
+
+n = save_to_json_file(n + sys.argv[1:], add_item.json)
