b484d621d7ae19960990d495278adc6b3e4b6246
David Alzate <illker@hotmail.com>
2021-01-21 03:37:28 +0000

    fix :rabbit:

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,5 +6,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text"""
 
-    with open(filename, "w", encoding="utf-8") as burger:
-        return burger.write(json.dumps(my_obj))
+    with open(filename, "w") as burger:
+        json.dump(my_obj, burger)
