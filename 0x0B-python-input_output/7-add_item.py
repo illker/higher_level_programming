549508c0a100126f5f674867dc9d9e4a67998591
David Alzate <illker@hotmail.com>
2021-01-21 03:17:24 +0000

    Load, add, save :rabbit:

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,8 +6,8 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json').load_from_json_file
 
 try:
-    n = load_from_json_file(add_item.json)
+    n = load_from_json_file("add_item.json")
 except:
     n = []
 
-n = save_to_json_file(n + sys.argv[1:], add_item.json)
+n = save_to_json_file(n + sys.argv[1:], "add_item.json")
