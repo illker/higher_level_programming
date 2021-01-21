1b5619f0d788577232f4bb4d3149e802fbcffd9a
David Alzate <illker@hotmail.com>
2021-01-21 03:47:37 +0000

    permissions :pig:

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" Load, add, save """
+"""Load, add, save"""
 import sys
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 try:
-    n = load_from_json_file("add_item.json")
+    file = load_from_json_file("add_item.json")
 except:
-    n = []
-for a in range(1, len(sys.argv)):
-    new_list.append(sys.argv[a])
-save_to_json_file(new_list, "add_item.json")
+    file = []
+file.extend(sys.argv[1:])
+save_to_json_file(file, "add_item.json")
