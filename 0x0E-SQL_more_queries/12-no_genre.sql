e646eff55a7874087fea778fcbdf6aa4574fd5ad
David Alzate <illker@hotmail.com>
2021-03-03 01:47:10 +0000

    null :warning:

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- mysql -uroot -p hbtn_0d_tvshows < hbtn_0d_tvshows.sql
+-- lists all shows contained in hbtn_0d_tvshows without a genre linked
+
+SELECT tv_shows.title, tv_show_genres.genre_id 
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
