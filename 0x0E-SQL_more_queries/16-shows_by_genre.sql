bcb2531562d6921c47ba6da4ec5c79145c4974e8
David Alzate <illker@hotmail.com>
2021-03-03 04:39:40 +0000

    List shows and genres :video_camera:

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- mysql -uroot -p hbtn_0d_tvshows < hbtn_0d_tvshows.sql
+--  lists all Comedy shows in the database hbtn_0d_tvshows
+
+SELECT tv_shows.title, tv_genres.name
+FROM tv_show_genres
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+RIGHT JOIN tv_shows
+ON tv_shows.id=tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_genres.name;
