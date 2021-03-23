77700ee97906d657a1797fb1fbcdd160ddda7786
David Alzate <illker@hotmail.com>
2021-03-23 03:43:54 +0000

    mult :mushroom: 2

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-let burger = parseInt(process.argv[2])
-if (isNaN(burger)) {
+if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < burger; i++) {
+  for (let i = 0; i < process.argv[2]; i++) {
     console.log('C is fun');
   }
 }
