df061f6334b64e71b5f3b23a258be2849ca6c935
David Alzate <illker@hotmail.com>
2021-03-24 15:20:03 +0000

    fix: :hankey:

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   const burger = process.argv.slice(2);
-  burger.sort();
+  burger.sort(function (a, b) { return a - b; });
   burger.reverse();
   console.log(burger[1]);
 }
