1da12cba5ef51e1e3991eac971522d1e63996ab7
David Alzate <illker@hotmail.com>
2021-03-24 15:15:44 +0000

    fix: S:punch:

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const burger = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const burger = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const i in burger) {
   console.log(burger[i]);
 }
