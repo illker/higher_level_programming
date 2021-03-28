4c10878f6193fb3debc778be0882926931b88b83
David Alzate <illker@hotmail.com>
2021-03-28 22:58:32 +0000

    filters :boom:

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script takes arg and displays values where matches the argument"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    bacon = db.cursor()
+    bacon.execute(
+        "SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(argv[4]))
+    burger = bacon.fetchall()
+    for bu in burger:
+        print(bu)
+    bacon.close()
+    db.close()
