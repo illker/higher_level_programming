964ca42606f7c13ff64456eaafadaa8ca2ec9611
David Alzate <illker@hotmail.com>
2021-03-29 01:20:49 +0000

    alchemy objects :microscope::gem:

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""script safe from MySQL injections!"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            argv[1],
+            argv[2],
+            argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    burger = session.query(State).order_by(State.id).first()
+    if burger is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(burger.id, burger.name))
+    session.close()
