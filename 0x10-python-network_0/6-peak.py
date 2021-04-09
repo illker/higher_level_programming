2b651d31ef5e77a6ee3212940a6dd9b2ab6531a6
illker <illker@hotmail.com>
2021-04-08 22:22:16 -0500

    peak :surfer:

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,5 +6,5 @@ def find_peak(list_of_integers):
     """ Method find peak"""
     if len(list_of_integers) == 0:
         return None
-    else:
-        return max(list_of_integers)
+    list_of_integers.sort()
+    return list_of_integers[-1]
