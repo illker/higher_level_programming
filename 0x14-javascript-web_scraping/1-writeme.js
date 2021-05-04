b857d29c5d3f72a9081d62bd4ac19101e580b96f
illker <illker@hotmail.com>
2021-05-04 16:06:17 -0500

    write :paw_prints:

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const fs = require('fs')
+const myArgs = process.argv.slice(2)
+
+fs.writeFile(myArgs[0], myArgs[1], 'utf-8', err => {
+  if (err) {
+    return console.error(err)
+  }
+})
