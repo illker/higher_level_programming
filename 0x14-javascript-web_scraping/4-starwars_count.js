d32eea2e7ef90d5b31c19b4f54a818c82433fad1
illker <illker@hotmail.com>
2021-05-04 19:08:08 -0500

    may 4 be with u :trollface:

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+const myArgs = process.argv.slice(2);
+const url = myArgs[0];
+const wedge = 'https://swapi-api.hbtn.io/api/people/18/';
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    let j = 0;
+    const burger = JSON.parse(body);
+    for (const bu of burger.results) {
+      for (const character of bu.characters) {
+        if (character.includes(wedge)) {
+          j++;
+        }
+      }
+    }
+    console.log(j);
+  }
+});
